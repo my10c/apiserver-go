6e5cda66cb33046950e24330cd5999b196fcb119
Luc Suryo <luc@suryo.com>
2023-07-24 17:01:12 -0600

    empty skeleton

---
+++ apiserver.go
@@ -0,0 +1,20 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2023, © Badassops LLC / Luc Suryo
+// All rights reserved.
+//
+// Version	:	0.1
+//
+
+package main
+
+import (
+	// "fmt"
+	// "os"
+	// "time"
+)
+
+func main() {
+
+	os.Exit(0)
+}
